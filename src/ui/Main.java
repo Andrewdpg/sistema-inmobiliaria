ce8cedbd80c6794ea1641525a002123f7e715801
Andres David Parra Garcia <1007560494@u.icesi.edu.co>
2022-10-21 12:14:54 -0500

    first commit

---
+++ src/ui/Main.java
@@ -0,0 +1,53 @@
+package ui;
+
+import model.Controller;
+import java.util.Scanner;
+
+public class Main{
+	private Scanner reader; 
+	private Controller controller;
+
+	public Main(){
+		controller = new Controller();
+	}
+	
+	public static void main(String[] args){
+		Main main = new Main();
+		int option = -1;
+
+		do{
+			option = getOptionShowMenu();
+			main.executeOption(option);
+		}while(option != 0);	
+	}
+
+
+	public Scanner getReader(){
+		return reader;
+	}
+
+	public int getOptionShowMenu(){
+		int option = 0; 
+		System.out.println("<<<<< Welcome to Real State >>>>>");
+		System.out.println(
+				"1. Add Person\n" +
+				"0. Exit. ");
+		option = reader.nextInt(); 
+
+		return option; 
+	}
+
+	public static int readOption(){
+
+	}
+
+	public void executeOption(int option){
+		switch(option){
+			case 1:
+				break;
+			default:
+				break;
+		}
+	}
+
+}
